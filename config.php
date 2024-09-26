b16f7a4f4ddcde44ca8a75b251ebd2d250206495
JorgeBigode <103458213+JorgeBigode@users.noreply.github.com>
2024-09-25 22:05:50 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-      $dbHost = 'localhost';
-      $dbUsername = 'root';
-      $dbPassword = '';
+      $dbHost = 'usually-faultless-lion.data-1.use1.tembo.io';
+      $dbUsername = 'postgres';
+      $dbPassword = 'Uk8wKkBvsquiCcLG';
       $dbName = 'formulario__cadastro';
 
       $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
